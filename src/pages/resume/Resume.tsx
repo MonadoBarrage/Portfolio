c4feb3c23f0e1dd5e6041d497af1f25673125943
MonadoBarrage <chenkatti@tutanota.com>
2025-12-22 12:37:15 -0500

    Made css styling choices to better fit mobile devices

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -5,7 +5,7 @@ import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import { pdfjs, Document, Page } from "react-pdf";
 import ResumeFile from "/src/assets/2025 Resume2.pdf";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoMdDownload } from "react-icons/io";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
@@ -16,6 +16,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 const Resume = () => {
   const pagesize = 2;
   const [index, setIndex] = useState(1);
+  const [pageWidth, setPageWidth] = useState<number>(0);
   const ChangePage = () => {
     var newindex = index + 1;
     if (newindex > pagesize) {
@@ -23,10 +24,33 @@ const Resume = () => {
     }
     setIndex(newindex);
   };
+  // const handleWindowSizeChange = () => {
+  //     const newpagewidth = window.innerWidth < 900 ? window.innerWidth-100: 0;
+  //       setPageWidth(newpagewidth);
+  //   };
+
+  // const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      var width = entries[0].contentRect.width;
+      // console.log("Width: ", width)
+      if (width < 920) {
+        width -= 100;
+      } else {
+        width = 0;
+      }
+      setPageWidth(Math.floor(width));
+    });
+
+    observer.observe(document.documentElement);
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <>
       <Document file={ResumeFile}>
-        <Page pageNumber={index}>
+        <Page pageNumber={index} width={pageWidth}>
           <a
             href={ResumeFile}
             // download="PDF-Document"
